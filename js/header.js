684c7ff15ecc7252b66d4e3116086d984f179788
Yash Chatim <yash.chatim@gmail.com>
2022-02-13 20:52:57 +0000

    Added navigation bar to header

--- js/header.js
+++ js/header.js
@@ -1,15 +1,30 @@
 class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <header class="w-100" style="position:fixed;top: 0;z-index: 1111;">
-                <div class="row mx-0 mt-2">
-                    <div class="col-6 mt-3">
-                        <a href="index.html">
-                            <img src="images/NavLogo.png" alt="" srcset=""  loading="lazy" width="106" >
-                        </a>
-                    </div>
+            <nav class="navbar navbar navbar-light w-100 mt-2" style="position:fixed;top: 0;z-index: 1111;">
+                <a href="index.html">
+                    <img src="images/NavLogo.png" alt="" srcset="" loading="lazy" width="106" >
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav" style="background-color: bisque; text-align: center;">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/index.html">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/about.html">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/projects.html">Projects</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/cc.html">Cultures Calendar</a>
+                        </li>
+                    </ul>
                 </div>
-            </header>
+            </nav>
         `
     }
 }
